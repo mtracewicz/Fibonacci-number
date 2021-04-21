03903b9e4a563acd4ba4f12c0fe7e14ad97c7608
Michał Tracewicz <m.tracewicz@gmail.com>
2021-04-21 23:52:33 +0200

    Added more tests.

--- fib/fib_test.go
+++ fib/fib_test.go
@@ -9,10 +9,24 @@ func TestIterative(t *testing.T) {
 	}
 }
 
+func TestIterativeLesserThen2(t *testing.T) {
+	got := IterativeFib(1)
+	if got != 1 {
+		t.Errorf("Fib(1) = %d; want 1", got)
+	}
+}
+
 func TestIterativeFibV2(t *testing.T) {
 	got := IterativeFibV2(20)
 	if got != 6765 {
 		t.Errorf("Fib(20) = %d; want 6765", got)
+	}
+}
+
+func TestIterativeFibV2LesserThen2(t *testing.T) {
+	got := IterativeFibV2(1)
+	if got != 1 {
+		t.Errorf("Fib(1) = %d; want 1", got)
 	}
 }
 
@@ -23,9 +37,23 @@ func TestRecursive(t *testing.T) {
 	}
 }
 
+func TestRecursiveLesserThen2(t *testing.T) {
+	got := RecursiveFib(1)
+	if got != 1 {
+		t.Errorf("Fib(1) = %d; want 1", got)
+	}
+}
+
 func TestSlices(t *testing.T) {
 	got := SlicesFib(20)
 	if got != 6765 {
 		t.Errorf("Fib(20) = %d; want 6765", got)
+	}
+}
+
+func TestSlicesLesserThen2(t *testing.T) {
+	got := SlicesFib(1)
+	if got != 1 {
+		t.Errorf("Fib(1) = %d; want 1", got)
 	}
 }
